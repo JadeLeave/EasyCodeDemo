4751802372e8dae921837bb6a674d9b211085f6b
angel <h221848@qq.com>
2022-02-24 23:01:01 +0800

    初始化项目

---
+++ src/main/java/com/jadex/easycodedemo/EasyCodeDemoApplication.java
@@ -0,0 +1,14 @@
+package com.jadex.easycodedemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasyCodeDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EasyCodeDemoApplication.class, args);
+        System.out.println("Test");
+    }
+
+}
